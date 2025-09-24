6855b4cf538aeed241dc20e5c445a9b4150aa436
Felix Díaz <felix.diaz.b@tecsup.edu.pe>
2025-09-24 13:08:04 -0500

    feat(project): Estructura inicial de carpetas por funcionalidad

---
+++ app/src/main/java/com/jhomilmotors/jhomilmotorsfff/ui/screens/confirmacion/ConfirmationScreen.kt
@@ -0,0 +1,16 @@
+package com.jhomilmotors.jhomilmotorsfff.ui.screens.confirmacion
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ConfirmationScreen() {
+    Text(text = "Pantalla de Confirmación de Pedido")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ConfirmationScreenPreview() {
+    ConfirmationScreen()
+}
