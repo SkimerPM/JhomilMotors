85a70b7d47af0ed997c77da55c720e4ce1af370d
SkimerPM <jose.sanchez.q@tecsup.edu.pe>
2025-09-25 18:13:37 -0500

    feat(login): implementar vista de inicio de sesión

---
+++ app/src/main/java/com/jhomilmotors/jhomilmotorsfff/ui/screens/login/Login.kt
@@ -0,0 +1,232 @@
+package com.jhomilmotors.jhomilmotorsfff.ui.screens.login
+
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.jhomilmotors.jhomilmotorsfff.R
+import com.jhomilmotors.jhomilmotorsfff.ui.theme.JhomilMotorsShopTheme
+
+// Aquí deberás reemplazar R.drawable.top_image_login y R.drawable.googl_logo con la ubicación real
+// de tus recursos.
+
+@Composable
+fun LoginScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.top_image_login),
+                contentDescription = "Imagen encabezado del Login",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(175.dp)
+                    .clipToBounds(),
+                contentScale = ContentScale.Crop
+            )
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 35.dp),
+            ) {
+                Text(
+                    text = "Inicio de Sesión",
+                    style = MaterialTheme.typography.headlineLarge,
+                    fontWeight = FontWeight.SemiBold,
+                    color = MaterialTheme.colorScheme.primary
+                )
+                Spacer(modifier = Modifier.height(17.dp))
+                Text(
+                    text = "Inicia Sesión con tu cuenta de JhomilShop",
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+                Spacer(modifier = Modifier.height(17.dp))
+                Text(
+                    text = "Correo",
+                    style = MaterialTheme.typography.bodyLarge,
+                    fontWeight = FontWeight.SemiBold,
+                    color = MaterialTheme.colorScheme.primary
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                TextField(
+                    value = "",
+                    onValueChange = {},
+                    placeholder = { Text("tu.email@ejemplo.com") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(62.dp),
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.09f),
+                        unfocusedContainerColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.03f),
+                        cursorColor = MaterialTheme.colorScheme.primary,
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                    ),
+                    shape = RoundedCornerShape(8.dp)
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = "Contraseña",
+                    style = MaterialTheme.typography.bodyLarge,
+                    fontWeight = FontWeight.SemiBold,
+                    color = MaterialTheme.colorScheme.primary,
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                TextField(
+                    value = "",
+                    onValueChange = {},
+                    placeholder = { Text("introduce tu contraseña aquí") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(62.dp),
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.09f),
+                        unfocusedContainerColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.03f),
+                        cursorColor = MaterialTheme.colorScheme.primary,
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent
+                    ),
+                    shape = RoundedCornerShape(8.dp)
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(
+                    onClick = {},
+                    modifier = Modifier
+                        .background(
+                            brush = Brush.horizontalGradient(
+                                colors = listOf(
+                                    MaterialTheme.colorScheme.primary,
+                                    MaterialTheme.colorScheme.primary.copy(0.8f)
+                                ),
+                                startX = Float.POSITIVE_INFINITY,
+                                endX = 0f
+                            ),
+                            shape = MaterialTheme.shapes.extraLarge
+                        )
+                        .fillMaxWidth()
+                        .height(62.dp),
+                    contentPadding = PaddingValues(),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Transparent
+                    )
+                ) {
+                    Text(
+                        text = "Iniciar Sesión",
+                        color = MaterialTheme.colorScheme.onPrimary
+                    )
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    HorizontalDivider(
+                        modifier = Modifier.weight(1f),
+                        color = MaterialTheme.colorScheme.outlineVariant
+                    )
+                    Text(
+                        text = "o",
+                        style = MaterialTheme.typography.bodySmall,
+                        fontWeight = FontWeight.Light,
+                        modifier = Modifier.padding(horizontal = 10.dp),
+                        color = MaterialTheme.colorScheme.onSurface
+                    )
+                    HorizontalDivider(
+                        modifier = Modifier.weight(1f),
+                        color = MaterialTheme.colorScheme.outlineVariant
+                    )
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(
+                    onClick = {},
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(62.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = MaterialTheme.colorScheme.outlineVariant
+                    )
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.googl_logo),
+                        contentDescription = "Logo de Google",
+                        modifier = Modifier
+                            .height(30.dp)
+                            .width(30.dp)
+                    )
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "¿No tienes una cuenta?",
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Text(
+                        text = "Registrarse",
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+fun PreviewLoginScreen(){
+    JhomilMotorsShopTheme{
+        LoginScreen()
+    }
+}
+
+
+@Composable
+@Preview(showBackground = true)
+fun PreviewLoginScreenClaro(){
+    JhomilMotorsShopTheme{
+        LoginScreen()
+    }
+}
